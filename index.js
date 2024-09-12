4260c8b4ffa6275f4e45153cb051eeb6eae49e4e
Felipe Paiva <sonyfpa@gmail.com>
2024-09-12 19:38:02 -0300

    add inicial menu

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-function start() {
-    console.log('comecou')
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+    
+    while(true){
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar Metas",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar Metas",
+                    value: "listar"
+                },
+                {
+                    nome: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+
+        switch(opcao) {
+            case "cadastrar":
+                console.log('Vamos Cadastrar')
+                break
+            case "listar":
+                console.log("Vamos Listar")
+                break
+            case "sair":
+                console.log("Ate a proxima!")
+                return
+        }
+    }
+    
 }
 
 start()
